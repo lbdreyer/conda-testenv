147b3c0544becf5afeb676d12271add282b66d5c
Laura Dreyer <laura_dreyer@yahoo.co.uk>
2015-12-10 10:50:56 +0000

    Missing comma in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
       author_email='lbdreyer@users.noreply.github.com',
       url='https://github.com/scitools/conda-testenv',
       packages=['conda_testenv', 'conda_testenv.tests', 'conda_testenv.tests.integration', 'conda_testenv.tests.integration.test_recipes'],
-      zip_safe=True
+      zip_safe=True,
       entry_points={
           'console_scripts': [
               'conda-testenv = conda_testenv.cli:main',
